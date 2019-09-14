5c62f71b193f76487963a1b54bac3d02fa04183f
Juan <juande-228@hotmail.com>
2019-09-14 11:36:35 -0500

    Pages Home, Detail porducts, Register and Login

---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Header from '../components/Header';
+import Product from '../components/Product';
+
+const Home = () => {
+    return (
+        <>
+            <Header />
+            <Product />
+        </>
+    );
+}
+
+export default Home;
